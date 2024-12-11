d98113f4fbe1ca1e33712d3b68d2554cad61185f
RohaTech <raklovetocode@gmail.com>
2024-12-11 21:53:44 +0300

    first commit

---
+++ Client/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ Client/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1 class="">Home View</h1>
+  </main>
+</template>
